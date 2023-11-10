fb73025f9254315f317e56c4f11b93d88ff5674e
RedstoneBommel <bommelsockenschlumpf@outlook.de>
2023-11-10 18:34:45 +0100

    bannedWords function 20231110

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from command import CommandsCommand, help, info, funny, games, joins
-from admin import AdminCommand, roles, log
+from admin import AdminCommand, AutoAdmin, roles, log
 from discord.ext import commands
 from dotenv import load_dotenv
 import discord
 import os
+import json
 
 class HomieBot(commands.Bot):
 
@@ -12,6 +13,7 @@ class HomieBot(commands.Bot):
     
     async def on_ready(self):
         await self.load_extension('admin.AdminCommand')
+        await self.load_extension('admin.AutoAdmin')
         # await self.load_extension('admin.roles')
         # await self.load_extension('admin.log')
 
@@ -30,6 +32,24 @@ class HomieBot(commands.Bot):
 
         if message.author == self.user:
             return
+        
+        messageWords = message.content.split()
+        with open('json/bannedWords.json', 'r') as admin_data:
+            bannedWords = json.load(admin_data)
+        bannedWords = bannedWords['bannedWords']
+        for i in messageWords:
+            if i in bannedWords:
+                await message.author.send(f"Please don't use this word: {i}")
+                with open('json/member.json', 'r') as admin_data:
+                    memberJSON = json.load(admin_data)
+                if message.author.name in memberJSON:
+                    print("check")
+                    memberJSON[message.author.name]['bannedWordsCounter'] += 1
+                else:
+                    memberJSON[message.author.name] = {"bannedWordsCounter": 1}
+                with open('json/member.json', 'w') as admin_data:
+                    json.dump(memberJSON, admin_data)
+
         
 load_dotenv()
 
