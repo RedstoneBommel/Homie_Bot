a3cb29acc0cf5b1e381df7f578b23542332af466
RedstoneBommel <bommelsockenschlumpf@outlook.de>
2023-12-15 16:55:55 +0100

    information command 20231215

--- command/info.py
+++ command/info.py
@@ -43,7 +43,35 @@ class info(commands.Cog):
                         return channel.name
         await interaction.response.send_message(f"{member.mention} there isn't any channel with rules. May ask an server admin.")
         return "no rules channel found"
-        
+    
+    # Information about every single user
+    @app_commands.command(name = "info", description = "Show information about users")
+    @has_role(everyone)
+    async def info(self, interaction: discord.Interaction, member: discord.Member = None):
+        if member is None:
+            member = interaction.user
+        with open("json/member.json", "r+") as member_data:
+            member_data = json.load(member_data)
+            if str(member.name) in member_data:
+                roles = member_data[str(member.name)]["roles"]
+                role_mentions = []
+                serverRoles = interaction.guild.roles
+                for role in roles:
+                    for i in serverRoles:
+                        if role == i.name:
+                            role_mentions.append(i.mention)
+            else:
+                return f"{member} didn't exist"
+        memberCard = discord.Embed(title = f"Information about {member.name}")
+        memberCard.color = discord.Color.from_rgb(0, 255, 0)
+        memberCard.set_thumbnail(url = member.display_avatar.url)
+        memberCard.set_author(name = interaction.user, icon_url = interaction.user.display_avatar.url)
+        memberCard.add_field(name = f"{member.mention}", value = f"ID: {member.id}", inline = False)
+        memberCard.add_field(name = "Member since:", value = member.joined_at.strftime("%d, %B, %Y"), inline = True)
+        memberCard.add_field(name = "Status:", value = str(member.desktop_status), inline = True)
+        memberCard.add_field(name = "Roles:", value = ', '.join(role_mentions), inline = False)
+        memberCard.set_footer(text = "This embed was created by Homie")
+        await interaction.response.send_message(embed = memberCard)
 
 async def setup(bot):
     await bot.add_cog(info(bot))
