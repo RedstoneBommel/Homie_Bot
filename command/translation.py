e8f73fc97f8e6264d51af4e636a173e78d8b0b9a
RedstoneBommel <bommelsockenschlumpf@outlook.de>
2023-12-29 16:54:47 +0100

    translat command finished 20231229

--- command/translation.py
+++ command/translation.py
@@ -1,7 +1,7 @@
 from googletrans import Translator
 from discord import app_commands
 from discord.ext import commands
-from discord import Color, Interaction, Role, Member
+from discord import Interaction
 import discord
 import googletrans
 
@@ -14,20 +14,26 @@ def has_role(role_name):
 class translation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.translator = Translator()
     everyone = "@everyone"
     @app_commands.command(name = "translate", description = "Translate messages to a specific language")
     @has_role(everyone)
     async def translate(self, interaction: Interaction, text: str, language: str = None):
-        translator = Translator()
         default_language = "en"
         if language is None:
             language = default_language
-        src_language = translator.detect(text)
-        if language not in googletrans.LANGUAGES:
-            await interaction.send_message("Unknown language")
+        src_language = self.translator.detect(text).lang
+        if src_language not in googletrans.LANGUAGES or language not in googletrans.LANGUAGES:
+            await interaction.response.send_message("Unknown language")
             return "Unknown language"
         else:
-            await interaction.send_message(translator.translate(text, src = src_language, dest = language))
+            # Aufschieben der Interaktion
+            await interaction.response.defer()
+            # Senden der Nachfolgenachricht
+            translated_text = self.translator.translate(text, src = src_language, dest = language).text
+            await interaction.followup.send(translated_text)
+
+
 
 async def setup(bot):
     await bot.add_cog(translation(bot))
