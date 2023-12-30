8eb431bcca0f84ffe731132c165b485613e7cc4d
RedstoneBommel <bommelsockenschlumpf@outlook.de>
2023-12-30 22:51:58 +0100

    important 20231230

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import discord
 import os
 import json
+import random
 
 class HomieBot(commands.Bot):
     def __init__(self, intents):
@@ -18,7 +19,7 @@ class HomieBot(commands.Bot):
         await self.load_extension('command.info')
         # await self.load_extension('command.funny')
         # await self.load_extension('command.games')
-        await self.load_extension('command.joins')
+        # await self.load_extension('command.joins')
         await self.load_extension('command.translation')
         print("sync extensions")
         await self.tree.sync()
@@ -51,6 +52,33 @@ class HomieBot(commands.Bot):
                     memberJSON[message.author.name] = {"bannedWordsCounter": 1}
                 with open('json/member.json', 'w') as admin_data:
                     json.dump(memberJSON, admin_data)
+    async def on_member_join(self, member):
+        welcomeMessages = [
+            "joined this server",
+            "welcome to this server",
+            "we hope you will have fun at this server",
+            "nice to meet you"
+        ]
+        welcomeMessage = random.choice(welcomeMessages)
+        with open("json/admin.json", "r") as admin_data:
+            admin_data = json.load(admin_data)
+            for channel in member.guild.channels:
+                if channel.id == admin_data["joins"]:
+                    break
+            else:
+                print("No Join Channel found")
+                return
+        await channel.send(member.name + welcomeMessage)
+        with open("json/member.json", "r+") as member_data:
+            member_data = json.load(member_data)
+            if member.name in member_data:
+                member_data.close()
+                return "Member is already in the register"
+            else:
+                member_data[member.name] = {"name": member.name, "id": member.id, "roles": [role.name for role in member.roles], "bannedWordsCounter": 0}
+                json.dump(member_data, member_data)
+                member_data.close()
+        await member.send("Check out our rule channel to be able to our server rules")
 load_dotenv()
 token = os.getenv('TOKEN')
 intents = discord.Intents.all()
