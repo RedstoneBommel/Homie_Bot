7bc191f2f3de61e0b101b56a296a9b38d9af0547
RedstoneBommel <bommelsockenschlumpf@outlook.de>
2023-12-30 18:14:53 +0100

    help command finished 20231230

--- command/help.py
+++ command/help.py
@@ -1,6 +1,7 @@
-from discord import app_commands, Button, ButtonStyle, Interaction, Embed
+from discord import app_commands, Interaction, ButtonStyle, Embed, Color
 from discord.ext import commands
 import discord
+from discord.ui import Button, View
 
 def has_role(role_name):
     def predicate(interaction: discord.Interaction):
@@ -11,22 +12,49 @@ def has_role(role_name):
 class help(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    class HelpButton(Button):
-        def __init__(self, label, command):
-            super().__init__(style=ButtonStyle.primary, label=label)
-            self.command = command
-        async def callback(self, interaction: Interaction):
-            await interaction.response.send_message(f"Der Befehl `{self.command}` macht folgendes: ...")
     everyone = "@everyone"
     @app_commands.command(name = "help", description = "Get a explanation of all commands")
     @has_role(everyone)
     async def help(self, interaction: Interaction):
-        embed = Embed(title = "Help", description = "Show help for all commands:")
-        buttons = [
-            Button("/info", "info"),
-            Button("/translate", "translate"),
-            Button("/rules", "rules"),
-        ]
-        await interaction.response.send_message(embed = embed, buttons = buttons)
+        info = Button(label = "/info", style = ButtonStyle.blurple)
+        rules = Button(label = "/rules", style = ButtonStyle.blurple)
+        translate = Button(label = "/translate", style = ButtonStyle.blurple)
+        async def button_callback_info(interaction):
+            info = Embed(title = "Info Command")
+            info.color = Color.from_rgb(255, 0, 0)
+            info.add_field(name = "Basic Command:", value = "**`/info`** \n This Command will show you general information about your self")
+            info.add_field(name = "Specific Command", value = "**`/info <username>`** \n This Command will show your general information about about this user")
+            info.set_footer(text = "This embed was created by Homie")
+            await interaction.response.send_message(embed = info, ephemeral=True, delete_after = 15)
+        async def button_callback_rules(interaction):
+            rules = Embed(title = "Rules Command")
+            rules.color = Color.from_rgb(255, 0, 0)
+            rules.add_field(name = "Command:", value = "**`/rules`** \n This Command will show you in which channel you could find the server rules")
+            rules.set_footer(text = "This embed was created by Homie")
+            await interaction.response.send_message(embed = rules, ephemeral=True, delete_after = 15)
+        async def button_callback_translate(interaction):
+            translate = Embed(title = "Translate Command")
+            translate.color = Color.from_rgb(255, 0, 0)
+            translate.add_field(name = "Basic Command:", value = "**`/translate <text>`** \n This Command will translate any text for into English")
+            translate.add_field(name = "Specific Command", value = "**`/translate <text> <language>`** \n This Command will translate any text into the language you choose")
+            translate.set_footer(text = "This embed was created by Homie")
+            await interaction.response.send_message(embed = translate, ephemeral=True, delete_after = 15)
+        
+        info.callback = button_callback_info
+        rules.callback = button_callback_rules
+        translate.callback = button_callback_translate
+
+        view = View()
+        view.add_item(info)
+        view.add_item(rules)
+        view.add_item(translate)
+
+        help = Embed(title = "Help")
+        help.color = Color.from_rgb(255, 0, 0)
+        help.add_field(name = "All Commands for Members", value = "Please use all commands carefully and don't spam unnecessarily in the chat")
+        help.set_footer(text = "This embed was created by Homie")
+
+        await interaction.response.send_message(embed = help, view = view, ephemeral=True, delete_after = 60)
+        
 async def setup(bot):
     await bot.add_cog(help(bot))
