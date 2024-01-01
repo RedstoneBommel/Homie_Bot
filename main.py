97b5736eac3a7fc035760169a38562b94678a1d3
RedstoneBommel <bommelsockenschlumpf@outlook.de>
2024-01-01 16:37:50 +0100

    join message updated 20240101

--- main.py
+++ main.py
@@ -56,36 +56,40 @@ class HomieBot(commands.Bot):
                 with open('json/member.json', 'w') as admin_data:
                     json.dump(memberJSON, admin_data)
     async def on_member_join(self, member):
-        if not member.bot:
-            welcomeMessages = [
-                "joined this server",
-                "welcome to this server",
-                "we hope you will have fun at this server",
-                "nice to meet you"
-            ]
-            welcomeMessage = random.choice(welcomeMessages)
-            with open("json/admin.json", "r") as admin_data:
-                admin_data = json.load(admin_data)
-                for channel in member.guild.channels:
-                    if channel.id == admin_data["joins"]:
-                        break
-                else:
-                    print("No Join Channel found")
-                    return
-                for rule in member.guild.channels:
-                    if rule.id == admin_data["rules"]:
-                        await channel.send(member.name + welcomeMessage + f" \n Check out our Server Rules: {rule.id.mention}")
-                    await channel.send(member.name + welcomeMessage)
-            with open("json/member.json", "r+") as member_data:
-                member_data = json.load(member_data)
-                if member.name in member_data:
-                    member_data.close()
-                    return "Member is already in the register"
-                else:
-                    member_data[member.name] = {"name": member.name, "id": member.id, "roles": [role.name for role in member.roles], "bannedWordsCounter": 0}
-                    json.dump(member_data, member_data)
-                    member_data.close()
-            await member.send("Check out our rule channel to be able to our server rules")
+        guild = member.guild
+        welcomeMessages = [
+            "joined this server",
+            "welcome to this server",
+            "we hope you will have fun at this server",
+            "nice to meet you"
+        ]
+        welcomeMessage = random.choice(welcomeMessages)
+        with open("json/admin.json", "r") as admin_data:
+            admin_data = json.load(admin_data)
+            for channel in guild.channels:
+                print(channel.id)
+                if channel.id == admin_data["joins"]:
+                    break
+            else:
+                print("No Join Channel found")
+                return
+            for rule in guild.channels:
+                if rule.id == admin_data["rules"]:
+                    await channel.send(f"{member.mention} {welcomeMessage} \n Check out our Server Rules: {rule.mention}")
+                    break
+            else:
+                await channel.send(member.mention + welcomeMessage)
+                return
+        with open("json/member.json", "r+") as member_data:
+            member_data = json.load(member_data)
+            if member.name in member_data:
+                member_data.close()
+                return "Member is already in the register"
+            else:
+                member_data[member.name] = {"name": member.name, "id": member.id, "roles": [role.name for role in member.roles], "bannedWordsCounter": 0}
+                json.dump(member_data, member_data)
+                member_data.close()
+        await member.send("Check out our rule channel to be able to our server rules")
 load_dotenv()
 token = os.getenv('TOKEN')
 intents = discord.Intents.all()
